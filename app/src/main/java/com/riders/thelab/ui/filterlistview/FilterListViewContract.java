b740860e86d3c4a5100b987b42a63c2eadd98ad1
MichaelStH <michael.sainthonore@gmail.com>
2020-12-05 19:32:45 +0100

    [feat/006-in-app-activities] Filter List View done

---
+++ app/src/main/java/com/riders/thelab/ui/filterlistview/FilterListViewContract.java
@@ -0,0 +1,13 @@
+package com.riders.thelab.ui.filterlistview;
+
+import com.riders.thelab.ui.base.BaseView;
+
+public interface FilterListViewContract {
+
+    interface View extends BaseView {
+    }
+
+    interface Presenter {
+    }
+
+}
---
+++ app/src/main/java/com/riders/thelab/ui/filterlistview/FilterListViewDetailActivity.java
@@ -0,0 +1,73 @@
+package com.riders.thelab.ui.filterlistview;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+import com.riders.thelab.R;
+import com.riders.thelab.ui.base.SimpleActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+@SuppressLint("NonConstantResourceId")
+public class FilterListViewDetailActivity extends SimpleActivity {
+
+    @BindView(R.id.rank)
+    TextView tvRank;
+    @BindView(R.id.country)
+    TextView tvCountry;
+    @BindView(R.id.population)
+    TextView tvPopulation;
+
+    String rank;
+    String country;
+    String population;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_filter_listview_detail);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(getString(R.string.activity_title_filter_list_view_detail));
+
+        ButterKnife.bind(this, findViewById(android.R.id.content));
+
+        getBundle();
+
+        loadData();
+    }
+
+    private void getBundle() {
+
+        // Retrieve data from MainActivity on item click event
+        Intent i = getIntent();
+        // Get the results of rank
+        rank = i.getStringExtra("rank");
+        // Get the results of country
+        country = i.getStringExtra("country");
+        // Get the results of population
+        population = i.getStringExtra("population");
+
+    }
+
+    private void loadData() {
+
+        // Load the results into the TextViews
+        tvRank.setText(rank);
+        tvCountry.setText(country);
+        tvPopulation.setText(population);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+}
