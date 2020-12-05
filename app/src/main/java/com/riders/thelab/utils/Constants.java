78e9b9a9941237e4f696ddda8efc3bfe5505e1ae
MichaelStH <michael.sainthonore@gmail.com>
2020-12-05 18:09:20 +0100

    [feat/006-in-app-activities] Color Activity done

--- app/src/main/java/com/riders/thelab/utils/Constants.java
+++ app/src/main/java/com/riders/thelab/utils/Constants.java
@@ -3,6 +3,7 @@ package com.riders.thelab.utils;
 import com.riders.thelab.R;
 import com.riders.thelab.data.local.model.App;
 import com.riders.thelab.data.local.model.RecyclerItem;
+import com.riders.thelab.ui.colors.ColorActivity;
 import com.riders.thelab.ui.palette.PaletteActivity;
 
 import java.util.ArrayList;
@@ -14,7 +15,7 @@ public class Constants {
 
     private static Constants instance;
 
-    public final List<App> activityItems;
+    private final List<App> activityItems;
     static ArrayList<RecyclerItem> recyclerItems;
 
     //REST client Base URL
@@ -53,52 +54,53 @@ public class Constants {
 
     public List<App> loadActivities() {
         List<App> list = new ArrayList<>();
-        /*activityItems.add(new ActivityItem("Highcharts API", "Testing Highcharts API for Android ...",
+        /*list.add(new App("Highcharts API", "Testing Highcharts API for Android ...",
                 R.drawable.logo_highcharts, HighChartsActivity.class));
 
-        activityItems.add(new ActivityItem("Android Services", "Android Services for any action...",
+        list.add(new App("Android Services", "Android Services for any action...",
                 R.drawable.logo_service, ServiceActivity.class));
 
-        activityItems.add(new ActivityItem("Fabric", "You can test crash of app and have a detailed notification...",
-                R.drawable.logo_fabric, FabricActivity.class));
+        list.add(new App("Fabric", "You can test crash of app and have a detailed notification...",
+                R.drawable.logo_fabric, FabricActivity.class));*/
 
-        activityItems.add(new ActivityItem("Colors", "Change color programmatically...",
+        list.add(new App("Colors", "Change color programmatically...",
                 R.drawable.logo_colors, ColorActivity.class));
 
-        activityItems.add(new ActivityItem("Recycler", "Recycler Basics and best practices...",
+        /*
+        list.add(new App("Recycler", "Recycler Basics and best practices...",
                 R.drawable.ic_launcher_background, RecyclerViewActivity.class));
 
-        activityItems.add(new ActivityItem("Transition", "Transition, shared elements...",
+        list.add(new App("Transition", "Transition, shared elements...",
                 R.drawable.transition_fade, TransitionActivity.class));
 
-        activityItems.add(new ActivityItem("Tabs", "ViewPager Fragments Tabs...",
+        list.add(new App("Tabs", "ViewPager Fragments Tabs...",
                 R.drawable.ic_tab_black_48dp, WorkingTabsActivity.class));
 
-        activityItems.add(new ActivityItem("Floating Labels", "Floating Labels Form...",
+        list.add(new App("Floating Labels", "Floating Labels Form...",
                 R.drawable.ic_label_outline_black_48dp, FloatingLabelsActivity.class));
 
-        activityItems.add(new ActivityItem("Location Google API", "Find user location and display coordinates...",
+        list.add(new App("Location Google API", "Find user location and display coordinates...",
                 R.drawable.ic_location_on_black_24dp, LocationGoogleAPIActivity.class));
 
-        activityItems.add(new ActivityItem("Contact List", "Contact List...",
+        list.add(new App("Contact List", "Contact List...",
                 R.drawable.ic_contacts_black_48dp, ContactListActivity.class));
 
-        activityItems.add(new ActivityItem("Location On Maps", "Display User location on map...",
+        list.add(new App("Location On Maps", "Display User location on map...",
                 R.drawable.ic_location_on_black_48dp, LocationOnMapsActivity.class));
 
-        activityItems.add(new ActivityItem("Schedule Job", "Own alarm to remind user...",
+        list.add(new App("Schedule Job", "Own alarm to remind user...",
                 R.drawable.ic_schedule_black_48dp, ScheduleJobActivity.class));
 
-        activityItems.add(new ActivityItem("Devices Informations", "Display device info...",
+        list.add(new App("Devices Informations", "Display device info...",
                 R.drawable.ic_perm_device_information_black_48dp, DeviceInformationsActivity.class));*/
 
-        list.add(new App("Palette", "Get different color from an image",
+        list.add(new App("Palette", "Get different color from an image...",
                 R.drawable.ic_palette_72, PaletteActivity.class));
 
-        /*activityItems.add(new ActivityItem("Filter ListView", "ListView with filter...",
+        /*list.add(new App("Filter ListView", "ListView with filter...",
                 R.drawable.ic_filter_list_black_48dp, FilterListViewActivity.class));
 
-        activityItems.add(new ActivityItem("Multi Pane", "Display content on split screen...",
+        list.add(new App("Multi Pane", "Display content on split screen...",
                 R.drawable.ic_aspect_ratio_black_48dp, MultiPaneActivity.class));*/
 /*
         listActivities.add( new ItemActivity("Contacts Database Activity", R.drawable.ic_perm_contact_calendar_black_48dp, ContactsDatabaseActivity.class) );
@@ -108,29 +110,29 @@ public class Constants {
         listActivities.add( new ItemActivity("RecyclerView Basics Activity", R.drawable.ic_filter_list_black_48dp, RecyclerBasicsActivity.class) );
         */
 
-        /*activityItems.add(new ActivityItem("RxJava", "Reactive Android...",
+        /*list.add(new App("RxJava", "Reactive Android...",
                 R.drawable.logo_rx_java, RxJavaActivity.class));
 
-        activityItems.add(new ActivityItem("Built-in Web View", "Display web view in activity directly...",
+        list.add(new App("Built-in Web View", "Display web view in activity directly...",
                 R.drawable.ic_aspect_ratio_black_48dp, BuiltInWebViewActivity.class));
 
-        activityItems.add(new ActivityItem("Songs Player (Work In Progress)", "Display content on split screen...",
+        list.add(new App("Songs Player (Work In Progress)", "Display content on split screen...",
                 R.mipmap.ic_launcher_round, SongPlayerActivity.class));
 
-        activityItems.add(new ActivityItem("Youtube", "Youtube look like...",
+        list.add(new App("Youtube", "Youtube look like...",
                 R.drawable.youtube_icon_like, YoutubeActivity.class));
 
-        activityItems.add(new ActivityItem("My Speech To Text", "Own speech to text implementation...",
+        list.add(new App("My Speech To Text", "Own speech to text implementation...",
                 R.drawable.ic_mic, SpeechToTextActivity.class));
 
-        activityItems.add(new ActivityItem("Activity Recognition", "Display user activity...",
+        list.add(new App("Activity Recognition", "Display user activity...",
                 R.drawable.ic_walking, ActivityRecognitionActivity.class));
 
-        activityItems.add(new ActivityItem("Weather App", "Current weather forecast.. in your city...",
+        list.add(new App("Weather App", "Current weather forecast.. in your city...",
                 R.drawable.openweathermap, WeatherActivity.class));
 
 
-        activityItems.add(new ActivityItem("WIP", "Coming soon...",
+        list.add(new App("WIP", "Coming soon...",
                 R.drawable.ic_warning, null));*/
 
         return list;
