9680a6eba0d922fab01da25358f24ad6059d2154
MichaelStH <michael.sainthonore@gmail.com>
2020-12-05 19:42:06 +0100

    [feat/006-in-app-activities] Filter List View using Presenter

--- app/src/main/java/com/riders/thelab/ui/filterlistview/FilterListViewPresenter.java
+++ app/src/main/java/com/riders/thelab/ui/filterlistview/FilterListViewPresenter.java
@@ -1,7 +1,11 @@
 package com.riders.thelab.ui.filterlistview;
 
+import com.riders.thelab.data.local.model.WorldPopulation;
 import com.riders.thelab.navigator.Navigator;
 import com.riders.thelab.ui.base.BasePresenterImpl;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -14,8 +18,37 @@ public class FilterListViewPresenter extends BasePresenterImpl<FilterListViewVie
     @Inject
     Navigator navigator;
 
+    String[] rank;
+    String[] country;
+    String[] population;
+
+
     @Inject
     FilterListViewPresenter() {
+
+        // Generate sample data
+        rank = new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+
+        country = new String[]{"China", "India", "United States",
+                "Indonesia", "Brazil", "Pakistan", "Nigeria", "Bangladesh",
+                "Russia", "Japan"};
+
+        population = new String[]{"1,354,040,000", "1,210,193,422",
+                "315,761,000", "237,641,326", "193,946,886", "182,912,000",
+                "170,901,000", "152,518,015", "143,369,806", "127,360,000"};
     }
 
+    @Override
+    public List<WorldPopulation> generatePopulationList() {
+
+        List<WorldPopulation> list = new ArrayList<>();
+
+        for (int i = 0; i < rank.length; i++) {
+            WorldPopulation wp = new WorldPopulation(rank[i], country[i], population[i]);
+            // Binds all strings into an array
+            list.add(wp);
+        }
+
+        return list;
+    }
 }
