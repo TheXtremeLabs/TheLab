1beabf575948b4b7e86f9821d560382419543a12
MichaelStH <michael.sainthonore@gmail.com>
2020-12-12 00:31:59 +0100

    [feat/006-in-app-activities] Contacts methods created , database, dao

--- app/src/main/java/com/riders/thelab/ui/contacts/ContactsContract.java
+++ app/src/main/java/com/riders/thelab/ui/contacts/ContactsContract.java
@@ -5,8 +5,26 @@ import com.riders.thelab.ui.base.BaseView;
 public interface ContactsContract {
 
     interface View extends BaseView {
+
+        void showLoader();
+
+        void hideLoader();
+
+        void onContactsFetchedSuccess();
+
+        void onContactsFetchedError();
+
+        void onNoContactRecordFound();
     }
 
     interface Presenter {
+
+        void getContactList();
+
+        void callDetailActivity();
+
+        void callAddNewContactActivity();
+
+        void addNewContact();
     }
 }
