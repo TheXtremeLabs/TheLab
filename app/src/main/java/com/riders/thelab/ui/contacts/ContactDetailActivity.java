701056439dd2c0bf0a6079d428aaafdd082c9b33
MichaelStH <michael.sainthonore@gmail.com>
2020-12-16 00:16:14 +0100

    [feat/006-in-app-activities] Back action

--- app/src/main/java/com/riders/thelab/ui/contacts/ContactDetailActivity.java
+++ app/src/main/java/com/riders/thelab/ui/contacts/ContactDetailActivity.java
@@ -7,8 +7,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.ViewCompat;
 
+import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.riders.thelab.R;
 import com.riders.thelab.ui.base.SimpleActivity;
@@ -22,8 +24,12 @@ public class ContactDetailActivity extends SimpleActivity {
 
     private Context mContext;
 
+    @BindView(R.id.app_bar_layout)
+    AppBarLayout appBarLayout;
     @BindView(R.id.contact_detail_collapsing_toolbar)
     CollapsingToolbarLayout mCollapsingToolbar;
+    @BindView(R.id.contact_detail_toolbar)
+    Toolbar toolbar;
     @BindView(R.id.iv_image_detail_toolbar)
     ImageView mImageDetailToolbar;
     @BindView(R.id.tv_name_detail)
@@ -57,13 +63,34 @@ public class ContactDetailActivity extends SimpleActivity {
         ViewCompat.setTransitionName(findViewById(R.id.app_bar_layout), String.valueOf(R.drawable.image5));
         supportPostponeEnterTransition();
 
-        //setSupportActionBar((Toolbar) findViewById(R.id.contact_detail_toolbar));
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         mCollapsingToolbar.setTitle(itemNameDetail);
         mCollapsingToolbar.setExpandedTitleColor(getResources().getColor(android.R.color.transparent));
 //        getSupportActionBar().setTitle(itemNameDetail);
 
+        appBarLayout.setExpanded(true);
+
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            boolean isShow = false;
+            int scrollRange = -1;
+
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.getTotalScrollRange();
+                }
+                if (scrollRange + verticalOffset == 0) {
+                    mCollapsingToolbar.setTitle(itemNameDetail);
+                    isShow = true;
+                } else if (isShow) {
+                    mCollapsingToolbar.setTitle(" ");
+                    isShow = false;
+                }
+            }
+        });
     }
 
     private void getBundle() {
