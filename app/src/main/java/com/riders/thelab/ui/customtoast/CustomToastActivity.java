cde0274907f650b5870c7b12e559c6ea7719f79e
MichaelStH <michael.sainthonore@gmail.com>
2021-02-05 22:39:35 +0100

    [feat/016-google-cloud-services] Google Places and Directions commented wip

--- app/src/main/java/com/riders/thelab/ui/customtoast/CustomToastActivity.java
+++ app/src/main/java/com/riders/thelab/ui/customtoast/CustomToastActivity.java
@@ -2,8 +2,13 @@ package com.riders.thelab.ui.customtoast;
 
 import android.animation.Animator;
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
+import android.graphics.Shader;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +20,7 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.progressindicator.CircularProgressIndicator;
@@ -31,6 +37,8 @@ import timber.log.Timber;
 
 @SuppressLint("NonConstantResourceId")
 public class CustomToastActivity extends AppCompatActivity {
+
+    private Context context;
 
     @BindView(R.id.display_toast_btn)
     MaterialButton displayToastButton;
@@ -50,6 +58,8 @@ public class CustomToastActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_custom_toast);
 
+        context = this;
+
         ButterKnife.bind(this);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -58,20 +68,69 @@ public class CustomToastActivity extends AppCompatActivity {
 
     @OnClick(R.id.button_custom)
     public void onDisplayToastButtonClicked() {
+        /*int[] colors = {
+                ContextCompat.getColor(context, R.color.success),
+                ContextCompat.getColor(context, R.color.warning),
+                ContextCompat.getColor(context, R.color.error)
+        };
+
+        int height = circularProgressIndicator.getHeight();
+        Shader shader= new LinearGradient(
+                0f, 0f, 0f, height,
+                colors,
+                null,
+                Shader.TileMode.REPEAT);
+
+        Paint paint = new Paint();
+        paint.setShader(shader);
+        circularProgressIndicator.setLayerPaint(paint);*/
 
         circularProgressIndicator.animate()
-                .setDuration(1500)
+                .setDuration(2000)
                 .alpha(1.0f)
                 .setListener(new Animator.AnimatorListener() {
                     @Override
                     public void onAnimationStart(Animator animation) {
                         circularProgressIndicator.setVisibility(View.VISIBLE);
                         circularProgressIndicator.setAlpha(0f);
+
+                        circularProgressIndicator.setIndicatorColor(
+                                ContextCompat.getColor(context, R.color.success),
+                                ContextCompat.getColor(context, R.color.warning),
+                                ContextCompat.getColor(context, R.color.error)
+                        );
                     }
 
                     @Override
                     public void onAnimationEnd(Animator animation) {
 
+                        new Handler().postDelayed(() -> {
+
+                            circularProgressIndicator.animate()
+                                    .setDuration(1500)
+                                    .alpha(0.0f)
+                                    .setListener(new Animator.AnimatorListener() {
+                                        @Override
+                                        public void onAnimationStart(Animator animation) {
+
+                                        }
+
+                                        @Override
+                                        public void onAnimationEnd(Animator animation) {
+                                            circularProgressIndicator.setVisibility(View.GONE);
+                                        }
+
+                                        @Override
+                                        public void onAnimationCancel(Animator animation) {
+
+                                        }
+
+                                        @Override
+                                        public void onAnimationRepeat(Animator animation) {
+
+                                        }
+                                    });
+                        }, 2000);
                     }
 
                     @Override
