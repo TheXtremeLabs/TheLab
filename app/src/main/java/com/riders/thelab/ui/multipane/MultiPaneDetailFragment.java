61f8ff57123ac671fc9c1473aecccb0d08ae0aa0
MichaelStH <michael.sainthonore@gmail.com>
2021-03-23 18:30:56 +0100

    [feat/020-multi-pane-fragment-tablet] multi pane tablet working

--- app/src/main/java/com/riders/thelab/ui/multipane/MultiPaneDetailFragment.java
+++ app/src/main/java/com/riders/thelab/ui/multipane/MultiPaneDetailFragment.java
@@ -6,16 +6,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.imageview.ShapeableImageView;
+import com.google.android.material.textview.MaterialTextView;
 import com.riders.thelab.R;
+import com.riders.thelab.data.local.model.Movie;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -27,27 +31,45 @@ public class MultiPaneDetailFragment extends Fragment {
 
     private Context mContext;
 
+    public static final String BUNDLE_MOVIE = "movie";
+
     //Views
-    @BindView(R.id.multi_pane_movie_item_image_detail)
-    ImageView ivMovieImage;
-    @BindView(R.id.multi_pane_movie_item_title_detail)
-    TextView tvTitleDetail;
-    @BindView(R.id.multi_pane_movie_item_genre_detail)
-    TextView tvGenreDetail;
-    @BindView(R.id.multi_pane_movie_item_year_detail)
-    TextView tvYearDetail;
-    @BindView(R.id.multi_pane_movie_item_loader_image)
+    private View rootView;
+    @BindView(R.id.multi_pane_movie_image_detail)
+    ShapeableImageView ivMovieImage;
+    @BindView(R.id.multi_pane_movie_title_detail)
+    MaterialTextView tvTitleDetail;
+    @BindView(R.id.multi_pane_movie_genre_detail)
+    MaterialTextView tvGenreDetail;
+    @BindView(R.id.multi_pane_movie_year_detail)
+    MaterialTextView tvYearDetail;
+    @BindView(R.id.multi_pane_movie_loader_image)
     ProgressBar progressBar;
     private Unbinder unbinder;
 
+    private static MultiPaneDetailFragment mFragmentInstance;
+
 
     public static MultiPaneDetailFragment newInstance() {
+        if (null == mFragmentInstance) {
+            mFragmentInstance = new MultiPaneDetailFragment();
+        }
+
+        return mFragmentInstance;
+    }
+
+    public static MultiPaneDetailFragment newInstance(Movie movie) {
 
         Bundle args = new Bundle();
+        args.putParcelable(BUNDLE_MOVIE, Parcels.wrap(movie));
 
         MultiPaneDetailFragment fragment = new MultiPaneDetailFragment();
         fragment.setArguments(args);
         return fragment;
+    }
+
+    public static MultiPaneDetailFragment getInstance() {
+        return mFragmentInstance;
     }
 
 
@@ -59,14 +81,13 @@ public class MultiPaneDetailFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         mContext = getActivity();
     }
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_multi_pane_detail, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_multi_pane_detail, container, false);
         unbinder = ButterKnife.bind(this, rootView);
         return rootView;
     }
@@ -88,16 +109,30 @@ public class MultiPaneDetailFragment extends Fragment {
     //  Class methods
     //
     ///////////////////////////////////////////
-    public void setTextOnFragment(String title, String genre, String year, String movieUrl) {
+    public void displayContent() {
+
+        Bundle bundle = this.getArguments();
+
+        if (null == bundle) {
+            Timber.e("Bundle is null - Cannot retrieve movie");
+            return;
+        }
+
+        Movie movie = Parcels.unwrap(bundle.getParcelable(BUNDLE_MOVIE));
+        setTextOnFragment(movie);
+    }
+
+    @SuppressLint("UseRequireInsteadOfGet")
+    private void setTextOnFragment(Movie movie) {
 
         if (progressBar != null) {
             progressBar.setVisibility(View.VISIBLE);
         }
 
-        Timber.e("URL RECEIVE : %s", movieUrl);
+        Timber.e("URL RECEIVE : %s", movie.getUrlThumbnail());
 
         Picasso.get()
-                .load(movieUrl)
+                .load(movie.getUrlThumbnail())
                 .into(ivMovieImage, new ImageLoadedCallback(progressBar) {
                     @Override
                     public void onSuccess() {
@@ -124,9 +159,9 @@ public class MultiPaneDetailFragment extends Fragment {
 
                 });
 
-        tvTitleDetail.setText(title);
-        tvGenreDetail.setText(genre);
-        tvYearDetail.setText(year);
+        tvTitleDetail.setText(movie.getTitle());
+        tvGenreDetail.setText(movie.getGenre());
+        tvYearDetail.setText(movie.getYear());
     }
     ///////////////////////////////////////////
     //
--- app/src/main/java/com/riders/thelab/ui/multipane/MultipaneActivity.java
+++ app/src/main/java/com/riders/thelab/ui/multipane/MultipaneActivity.java
@@ -1,17 +1,32 @@
 package com.riders.thelab.ui.multipane;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 
 import com.riders.thelab.R;
+import com.riders.thelab.core.utils.LabCompatibilityManager;
+import com.riders.thelab.data.local.model.Movie;
 import com.riders.thelab.ui.base.BaseActivity;
 
-public class MultipaneActivity extends BaseActivity<MultipaneView> {
+import org.parceler.Parcels;
 
+import timber.log.Timber;
+
+public class MultipaneActivity extends BaseActivity<MultipaneView>
+        implements MultiPaneMainFragment.OnItemSelectedListener {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        if (LabCompatibilityManager.isTablet(this))
+            //Force screen orientation to Landscape mode
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        else
+            //Force screen orientation to Portrait mode
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
         setContentView(R.layout.activity_multi_pane);
         super.onCreate(savedInstanceState);
     }
@@ -34,4 +49,18 @@ public class MultipaneActivity extends BaseActivity<MultipaneView> {
         super.onDestroy();
     }
 
+    @Override
+    public void onMovieItemSelected(Movie movie) {
+        MultiPaneDetailFragment fragment = MultiPaneDetailFragment.getInstance();
+
+        if (null != fragment) {
+            Bundle args = new Bundle();
+            args.putParcelable(MultiPaneDetailFragment.BUNDLE_MOVIE, Parcels.wrap(movie));
+            fragment.setArguments(args);
+
+            fragment.displayContent();
+        } else {
+            Timber.e("Something went wrong while loading data into fragment");
+        }
+    }
 }
