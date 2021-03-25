6736bc9e5621ae41696fd31e099d6eea00dfdbb5
MichaelStH <michael.sainthonore@gmail.com>
2021-03-25 16:52:15 +0100

    [feat/021-refactor-colors] colors refactor done

--- app/src/main/java/com/riders/thelab/ui/colors/ColorActivity.java
+++ app/src/main/java/com/riders/thelab/ui/colors/ColorActivity.java
@@ -1,17 +1,16 @@
 package com.riders.thelab.ui.colors;
 
-import android.animation.ObjectAnimator;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.annotation.Nullable;
-import androidx.core.content.ContextCompat;
-import androidx.vectordrawable.graphics.drawable.ArgbEvaluator;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textview.MaterialTextView;
 import com.riders.thelab.R;
+import com.riders.thelab.core.utils.LabAnimationsManager;
+import com.riders.thelab.core.utils.LabColorsManager;
 import com.riders.thelab.ui.base.SimpleActivity;
 
 import java.util.Random;
@@ -19,14 +18,13 @@ import java.util.Random;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import timber.log.Timber;
 
 @SuppressLint("NonConstantResourceId")
 public class ColorActivity extends SimpleActivity {
 
+    // Views
     @BindView(R.id.target_color_textView)
     MaterialTextView targetColorTextView;
-
     @BindView(R.id.change_color_button)
     MaterialButton changeColorButton;
 
@@ -36,9 +34,6 @@ public class ColorActivity extends SimpleActivity {
     int toColor;
 
     int randomColor;
-
-    private int shortAnimationDuration;
-
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -50,68 +45,37 @@ public class ColorActivity extends SimpleActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(getString(R.string.activity_title_colors));
 
-
-        // Retrieve and cache the system's default "short" animation time.
-        shortAnimationDuration = getResources().getInteger(
-                android.R.integer.config_shortAnimTime);
-
-
-        colors = new int[]{
-                ContextCompat.getColor(this, R.color.white),
-                ContextCompat.getColor(this, R.color.red),
-                ContextCompat.getColor(this, R.color.blue),
-                ContextCompat.getColor(this, R.color.green),
-                ContextCompat.getColor(this, R.color.orange),
-                ContextCompat.getColor(this, R.color.purple),
-                ContextCompat.getColor(this, R.color.yellow),
-                ContextCompat.getColor(this, R.color.teal_700)
-        };
+        colors = LabColorsManager.getDefaultColors(this);
     }
 
     @OnClick(R.id.change_color_button)
     public void changeColor(View view) {
 
+        // Get a random color
         randomColor = colors[new Random().nextInt(colors.length)];
 
-        Timber.d("color : %d ", randomColor);
-
+        // Set as target color to use in animation
         toColor = randomColor;
+        // Get current TextView color
         fromColor = targetColorTextView.getCurrentTextColor();
-        applyColorFade(targetColorTextView, fromColor, toColor);
+        // Apply fade color transition to TextView
+        LabAnimationsManager.applyFadeColorAnimationToView(
+                targetColorTextView,
+                fromColor,
+                toColor,
+                LabAnimationsManager
+                        .getInstance()
+                        .getShortAnimationDuration());
 
+        // Get current button color
         fromColor = changeColorButton.getCurrentTextColor();
-        applyColorFade(changeColorButton, fromColor, toColor);
-    }
-
-
-    @SuppressLint("RestrictedApi")
-    private void applyColorFade(View view, int fromColor, int toColor) {
-        ObjectAnimator fadeAnimator = null;
-
-        if (view instanceof MaterialTextView) {
-            fadeAnimator = ObjectAnimator.ofObject(
-                    view,
-                    "textColor",
-                    new ArgbEvaluator(),
-                    fromColor,
-                    toColor
-            );
-        }
-
-        if (view instanceof MaterialButton) {
-            fadeAnimator = ObjectAnimator.ofObject(
-                    view,
-                    "backgroundColor",
-                    new ArgbEvaluator(),
-                    fromColor,
-                    toColor
-            );
-        }
-
-        if (null != fadeAnimator) {
-
-            fadeAnimator.setDuration(shortAnimationDuration);
-            fadeAnimator.start();
-        }
+        // Apply fade color transition to Button
+        LabAnimationsManager.applyFadeColorAnimationToView(
+                changeColorButton,
+                fromColor,
+                toColor,
+                LabAnimationsManager
+                        .getInstance()
+                        .getShortAnimationDuration());
     }
 }
