0ffe0f9f29338ab8a5a101e0977b1fea5be67fcf
MichaelStH <michael.sainthonore@gmail.com>
2021-05-05 21:17:53 +0200

    [feat/023-app-refactoring] Clean code

--- app/src/main/java/com/riders/thelab/ui/recycler/RecyclerClickListener.java
+++ app/src/main/java/com/riders/thelab/ui/recycler/RecyclerClickListener.java
@@ -1,17 +1,9 @@
 package com.riders.thelab.ui.recycler;
 
 import com.google.android.material.imageview.ShapeableImageView;
-import com.riders.thelab.data.local.model.RecyclerItem;
 import com.riders.thelab.data.remote.dto.Artist;
 
 public interface RecyclerClickListener {
-    /*void onRecyclerClick(RecyclerItem item);
-
-    void onDetailClick(RecyclerItem item, ShapeableImageView sharedImageView, int position);
-
-    void onDeleteClick(RecyclerItem item, int position);*/
-
-
     void onRecyclerClick(Artist artist);
 
     void onDetailClick(Artist artist, ShapeableImageView sharedImageView, int position);
