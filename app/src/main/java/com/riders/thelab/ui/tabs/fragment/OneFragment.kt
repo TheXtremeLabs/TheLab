fe54cca6183b288651d5edddae4ec582cf81df62
MichaelStH <michael.sainthonore@gmail.com>
2021-05-19 22:06:07 +0200

    [feat/029-the-lab-6-kt] TabsActivity migrated to kotlin done

---
+++ app/src/main/java/com/riders/thelab/ui/tabs/fragment/OneFragment.kt
@@ -0,0 +1,28 @@
+package com.riders.thelab.ui.tabs.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.riders.thelab.databinding.FragmentOneBinding
+
+class OneFragment : Fragment() {
+    companion object {
+        fun newInstance(): OneFragment {
+            val args = Bundle()
+            val fragment = OneFragment()
+            fragment.arguments = args
+            return fragment
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val viewBinding = FragmentOneBinding.inflate(inflater, container, false)
+        return viewBinding.root
+    }
+}
