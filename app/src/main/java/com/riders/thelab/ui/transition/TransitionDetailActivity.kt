00d7cee65fe6e28f2e8f3445d89854d5fe321169
MichaelStH <michael.sainthonore@gmail.com>
2021-05-20 07:54:02 +0200

    [feat/029-the-lab-6-kt] TransitionActivity migrated to kotlin done

---
+++ app/src/main/java/com/riders/thelab/ui/transition/TransitionDetailActivity.kt
@@ -0,0 +1,41 @@
+package com.riders.thelab.ui.transition
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.riders.thelab.R
+import com.riders.thelab.databinding.ActivityTransitionDetailBinding
+
+class TransitionDetailActivity : AppCompatActivity() {
+
+    lateinit var viewBinding: ActivityTransitionDetailBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewBinding = ActivityTransitionDetailBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        supportActionBar?.title = getString(R.string.activity_title_transition_detail)
+
+        viewBinding.buttonDetail.setOnClickListener {
+            onBackPressed()
+        }
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                supportFinishAfterTransition()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finishAfterTransition()
+    }
+}
