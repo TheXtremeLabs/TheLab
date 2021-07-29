d32cb75e8602034d2e1633563ac1408143a8613d
MichaelStH <michael.sainthonore@gmail.com>
2021-07-29 20:18:14 +0200

    [feat/031-stream-drive] : MainActivity data binding used + Glide use through UIManager class

--- app/src/main/java/com/riders/thelab/ui/mainactivity/MainActivityViewHolder.kt
+++ app/src/main/java/com/riders/thelab/ui/mainactivity/MainActivityViewHolder.kt
@@ -6,16 +6,11 @@ import android.graphics.drawable.Drawable
 import android.view.View
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.Target
 import com.riders.thelab.R
+import com.riders.thelab.core.utils.LabGlideListener
 import com.riders.thelab.core.utils.UIManager
 import com.riders.thelab.data.local.model.app.App
 import com.riders.thelab.databinding.RowMainAppItemBinding
-import com.riders.thelab.utils.Validator
 
 class MainActivityViewHolder constructor(
     private val context: Context,
@@ -24,67 +19,51 @@ class MainActivityViewHolder constructor(
 
 
     fun bindData(app: App) {
+
+        viewBinding.app = app
+
         // Load left icon drawable
         viewBinding.rowIconImageView?.let {
-            Glide.with(context)
-                .load(if (0 != app.appIcon) app.appIcon else app.appDrawableIcon)
-                .listener(object : RequestListener<Drawable> {
-                    override fun onLoadFailed(
-                        e: GlideException?, model: Any,
-                        target: Target<Drawable>, isFirstResource: Boolean
-                    ): Boolean {
-                        return false
+            UIManager.loadImage(
+                context,
+                (if (0 != app.appIcon) app.appIcon else app.appDrawableIcon)!!,
+                it,
+                LabGlideListener(onLoadingSuccess = { resource: Drawable? ->
+                    if (0 != app.appIcon && app.appTitle == "Palette") {
+                        val myBitmap = UIManager.drawableToBitmap(resource!!)
+                        val newBitmap = UIManager.addGradientToImageView(context, myBitmap)
+                        it.setImageDrawable(
+                            BitmapDrawable(context.resources, newBitmap)
+                        )
+                        return@LabGlideListener true
                     }
-
-                    override fun onResourceReady(
-                        resource: Drawable, model: Any,
-                        target: Target<Drawable>, dataSource: DataSource,
-                        isFirstResource: Boolean
-                    ): Boolean {
-                        if (0 != app.appIcon && app.appTitle == "Palette") {
-                            val myBitmap = (resource as BitmapDrawable).bitmap
-                            val newBitmap = UIManager.addGradientToImageView(context, myBitmap)
-                            it.setImageDrawable(
-                                BitmapDrawable(context.resources, newBitmap)
-                            )
-                            return true
-                        }
-                        if (0 != app.appIcon && app.appTitle == "WIP") {
-                            it.setImageDrawable(
-                                ContextCompat.getDrawable(context, R.drawable.logo_testing)
-                            )
-                            viewBinding.arrowIcon?.visibility = View.GONE
-                            return true
-                        }
-                        return false
+                    if (0 != app.appIcon && app.appTitle == "WIP") {
+                        it.setImageDrawable(
+                            ContextCompat.getDrawable(context, R.drawable.logo_testing)
+                        )
+                        viewBinding.arrowIcon?.visibility = View.GONE
+                        return@LabGlideListener true
                     }
+                    false
                 })
-                .into(it)
+            )
         }
 
-
-        bindTitleAndDescription(app)
-
-        // Load background image
-        Glide.with(context)
-            .load(if (null != app.appActivity) app.appIcon else app.appDrawableIcon)
-            .into(viewBinding.ivRowItemBackground)
+        loadBackgroundImage(app)
 
     }
 
     fun bindTabletData(app: App) {
-        // Load background image
-        Glide.with(context)
-            .load(if (null != app.appActivity) app.appIcon else app.appDrawableIcon)
-            .into(viewBinding.ivRowItemBackground)
-
-        bindTitleAndDescription(app)
+        viewBinding.app = app
+        loadBackgroundImage(app)
     }
 
-    private fun bindTitleAndDescription(app: App) {
-        viewBinding.rowTitleTextView.text =
-            if (!Validator.isEmpty(app.appTitle)) app.appTitle else app.appName
-        viewBinding.rowDescriptionTextView.text =
-            if (!Validator.isEmpty(app.appVersion)) app.appVersion else app.appDescription
+    private fun loadBackgroundImage(app: App) {
+        // Load background image
+        UIManager.loadImage(
+            context,
+            (if (null != app.appActivity) app.appIcon else app.appDrawableIcon)!!,
+            viewBinding.ivRowItemBackground
+        )
     }
 }
