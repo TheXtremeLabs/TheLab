656fbdee34c9b15595928b3cee6b86d85955ff10
MichaelStH <michael.sainthonore@gmail.com>
2021-07-30 14:47:35 +0200

    [feat/031-stream-drive]: News View Model change use Repository interface

--- app/src/main/java/com/riders/thelab/ui/mainactivity/fragment/news/NewsFragment.kt
+++ app/src/main/java/com/riders/thelab/ui/mainactivity/fragment/news/NewsFragment.kt
@@ -82,7 +82,7 @@ class NewsFragment : Fragment(), View.OnClickListener {
                     setupCards(recentApps)
                 })
 
-        mNewsViewModel.fetchRecentApps(recentAppsNames)
+        mNewsViewModel.fetchRecentApps(requireContext(), recentAppsNames)
     }
 
     override fun onDestroyView() {
@@ -112,8 +112,6 @@ class NewsFragment : Fragment(), View.OnClickListener {
     ) {
         if (app.appDrawableIcon != null) {
             imageView.setImageDrawable(app.appDrawableIcon)
-        } else {
-            imageView.setImageResource(app.appIcon)
         }
         textView.text = if (app.appName != null) app.appName else app.appTitle
     }
