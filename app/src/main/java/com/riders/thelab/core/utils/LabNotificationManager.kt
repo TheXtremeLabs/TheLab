1d1e2721546e3ae32cd5227245b372df557e9964
MichaelStH <michael.sainthonore@gmail.com>
2021-09-16 19:02:53 +0200

    [feat/037-lab-notification] : Wip notification media player buttons event

--- app/src/main/java/com/riders/thelab/core/utils/LabNotificationManager.kt
+++ app/src/main/java/com/riders/thelab/core/utils/LabNotificationManager.kt
@@ -5,8 +5,15 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.os.Build
+import android.support.v4.media.session.MediaControllerCompat
+import android.support.v4.media.session.MediaSessionCompat
+import android.support.v4.media.session.PlaybackStateCompat
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.media.session.MediaButtonReceiver
 import com.riders.thelab.R
+import com.riders.thelab.core.service.MusicMediaPlaybackService
+import com.riders.thelab.data.local.model.music.SongModel
 import com.riders.thelab.utils.Constants
 import timber.log.Timber
 
@@ -45,6 +52,7 @@ class LabNotificationManager private constructor() {
             context: Context,
             mPendingIntent: PendingIntent
         ): NotificationCompat.Builder {
+            Timber.d("buildMainNotification()")
             return NotificationCompat.Builder(context, Constants.NOTIFICATION_CHANNEL_ID)
                 .apply {
                     setSmallIcon(R.mipmap.ic_lab_six_round)
@@ -62,6 +70,96 @@ class LabNotificationManager private constructor() {
                     setAutoCancel(false)
                     setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
                 }
+        }
+
+        fun displayMusicNotification(
+            context: Context,
+            mediaSession: MediaSessionCompat,
+            controller: MediaControllerCompat,
+            mServiceMusic: MusicMediaPlaybackService,
+            songModel: SongModel
+        ) {
+            Timber.d("displayMusicNotification()")
+
+            val mediaMetadata = controller.metadata
+            val description = mediaMetadata?.description
+
+            val notification =
+                NotificationCompat.Builder(context, Constants.NOTIFICATION_MUSIC_CHANNEL_ID).apply {
+
+                    // Add the metadata for the currently playing track
+                    setContentTitle(description?.title)
+                    setContentText(description?.subtitle)
+                    setSubText(description?.description)
+                    setLargeIcon(description?.iconBitmap)
+
+                    // Enable launching the player by clicking the notification
+                    setContentIntent(controller.sessionActivity)
+
+                    // Stop the service when the notification is swiped away
+                    setDeleteIntent(
+                        MediaButtonReceiver.buildMediaButtonPendingIntent(
+                            context,
+                            PlaybackStateCompat.ACTION_STOP
+                        )
+                    )
+
+                    // Show controls on lock screen even when user hides sensitive content.
+                    setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+
+                    // Add an app icon and set its accent color
+                    // Be careful about the color
+                    setSmallIcon(R.drawable.ic_music)
+
+                    // Add a pause button
+                    addAction(
+                        NotificationCompat.Action(
+                            R.drawable.ic_pause,
+                            context.getString(R.string.action_pause),
+                            MediaButtonReceiver.buildMediaButtonPendingIntent(
+                                context,
+                                PlaybackStateCompat.ACTION_PLAY_PAUSE
+                            )
+                        )
+                    )
+
+
+                    // Add media control buttons that invoke intents in your media service
+                    //.addAction(R.drawable.ic_previous, "Previous", prevPendingIntent) // #0
+                    //.addAction(R.drawable.ic_pause, "Pause", pausePendingIntent) // #1
+                    //.addAction(R.drawable.ic_next, "Next", nextPendingIntent) // #2
+                    // Apply the media style template
+                    setStyle(
+                        // Take advantage of MediaStyle features
+                        androidx.media.app.NotificationCompat.MediaStyle()
+                            .setShowActionsInCompactView(0 /* #1: pause button */)
+                            .setMediaSession(mediaSession.sessionToken)
+
+                            // Add a cancel button
+                            .setShowCancelButton(true)
+                            .setCancelButtonIntent(
+                                MediaButtonReceiver.buildMediaButtonPendingIntent(
+                                    context,
+                                    PlaybackStateCompat.ACTION_STOP
+                                )
+                            )
+                    )
+                    setContentTitle(songModel.name)
+                    setContentText(songModel.path)
+                    setLargeIcon(
+                        UIManager.getDrawable(
+                            context,
+                            R.drawable.logo_colors
+                        )!!
+                    )
+                }
+
+            with(NotificationManagerCompat.from(context)) {
+                // notificationId is a unique int for each notification that you must define
+                notify(Constants.NOTIFICATION_MUSIC_ID, notification.build())
+                // Provide a unique integer for the "notificationId" of each notification.
+                mServiceMusic.startForeground(Constants.NOTIFICATION_MUSIC_ID, notification.build())
+            }
         }
     }
 }
