5c38ade8f4061a2f4a9bb25a35e0a80fffc578ed
MichaelStH <michael.sainthonore@gmail.com>
2022-01-28 21:51:40 +0100

    [feat/048-settings] : Settings wip

---
+++ app/src/main/java/com/riders/thelab/ui/settings/SettingsActivity.kt
@@ -0,0 +1,23 @@
+package com.riders.thelab.ui.settings
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.riders.thelab.R
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.settings_container, SettingsFragment())
+            .commit()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+}
