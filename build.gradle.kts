543539a23d84d8cb76040d035a4b3f4277296f35
MichaelStH <michael.sainthonore@gmail.com>
2023-01-12 19:08:22 +0100

    [feat/056-gradle-kts] : migrate gradle groovy files to kotlin gradle kts

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    dependencies {
+        // Kotlin plugin
+        classpath(BuildPlugins.kotlin)
+        classpath(BuildPlugins.kotlinSerialization)
+
+        // Gradle plugin
+        classpath(BuildPlugins.gradle)
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+
+        // Hilt
+        classpath(BuildPlugins.hilt)
+        // Google Services
+        classpath(BuildPlugins.playServices)
+        //  Crashlytics Gradle plugin
+        classpath(BuildPlugins.crashlytics)
+        // Performance Monitoring plugin
+        classpath(BuildPlugins.performances)
+        // Navigation
+        classpath(BuildPlugins.navigation)
+    }
+}
+
+plugins {
+    id("com.android.application") version "7.3.1" apply false
+    id("com.android.library") version "7.3.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.7.20" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.7.20" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
+
+
+tasks.register("printVersionName") {
+    doLast {
+//        android.defaultConfig.versionName
+//        print(rootProject.versionName)
+    }
+}
