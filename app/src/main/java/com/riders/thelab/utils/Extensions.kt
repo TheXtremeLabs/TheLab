37fecd0e2275b19eeabfb33fd2242c13e5c31ddb
MichaelStH <michael.sainthonore@gmail.com>
2023-05-23 11:40:39 +0200

    [feat/076-weather]: ui improvements + remove asyncimagepainter extensions causes app freeze on image loading

--- app/src/main/java/com/riders/thelab/utils/Extensions.kt
+++ app/src/main/java/com/riders/thelab/utils/Extensions.kt
@@ -3,10 +3,18 @@ package com.riders.thelab.utils
 import android.graphics.drawable.Drawable
 import coil.compose.AsyncImagePainter
 import kotlinx.coroutines.runBlocking
+import kotlin.coroutines.CoroutineContext
 
 // painter.loadImage() -> Drawable
 fun AsyncImagePainter.loadImage(): Drawable =
     runBlocking {
+        imageLoader
+            .execute(request)
+            .drawable!!
+    }
+
+fun AsyncImagePainter.loadImage(coroutineContext: CoroutineContext): Drawable =
+    runBlocking(coroutineContext) {
         imageLoader
             .execute(request)
             .drawable!!
