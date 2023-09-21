466d73e4798f3b99cf3165591b55e9af18dc1589
MichaelStH <michael.sainthonore@gmail.com>
2023-09-21 22:20:18 +0200

    [feat/092-music-recognition]: update to version 12.0.12

--- build-logic/config/src/main/kotlin/AndroidConfiguration.kt
+++ build-logic/config/src/main/kotlin/AndroidConfiguration.kt
@@ -10,7 +10,7 @@ object AndroidConfiguration {
 
     object Application {
         const val code: Int = 1
-        val version: Version = Version(major = 12, minor = 0, patch = 11)
+        val version: Version = Version(major = 12, minor = 0, patch = 12)
     }
 }
 
