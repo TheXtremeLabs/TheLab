3bdab89488c9cbce52230782156fb72e0ab902f3
MichaelStH <michael.sainthonore@gmail.com>
2023-11-19 04:36:43 +0100

    [feat/093-settings]: created settings module

---
+++ feature/settings/build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    id("thelab.android.feature")
+    id("thelab.android.library.compose")
+    id("thelab.android.hilt")
+    kotlin("kapt")
+}
+
+android {
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    namespace = "com.riders.thelab.feature.settings"
+}
+
+dependencies {
+    ///////////////////////////////////
+    // Project
+    ///////////////////////////////////
+    implementation(project(":core:analytics"))
+    implementation(project(":core:data"))
+    implementation(project(":core:common"))
+    implementation(project(":core:ui"))
+
+
+    ///////////////////////////////////
+    // General Dependencies
+    ///////////////////////////////////
+    // AndroidX
+    implementation(libs.androidx.browser)
+    implementation(libs.androidx.core.ktx)
+
+    // Worker & concurrent
+    api(libs.androidx.work.ktx)
+    api(libs.androidx.work.multiprocess)
+    api(libs.androidx.concurrent)
+    androidTestApi(libs.androidx.work.testing)
+
+    /* Hilt - We are going to use hilt.android which includes
+     * support for Activity and fragment injection so we need to include
+     * the following dependencies */
+    // Hilt
+    //implementation(Dependencies.hilt)
+    //kapt(Dependencies.hiltCompiler)
+    kapt(libs.hilt.compiler)
+    implementation(libs.hilt.ext.work)
+    kapt(libs.hilt.ext.compiler)
+
+    // OkHttp
+    // define a BOM and its version
+    implementation(platform(libs.okhttp.bom))
+    // define any required OkHttp artifacts without version
+    implementation(libs.okhttp)
+
+    implementation(libs.zxing.core)
+    implementation(libs.zxing.parent)
+    implementation(libs.zxing.android.core)
+    implementation(libs.zxing.android.integration)
+
+    androidTestImplementation(project(":core:testing"))
+}
