171be34a16c51b3ae65618b13ff85518afb60d20
MSHonore <mickael.saint-honore@chronopost.fr>
2023-12-09 13:38:49 +0100

    [develop]: update baseViewModel

--- core/ui/src/main/java/com/riders/thelab/core/ui/compose/base/BaseViewModel.kt
+++ core/ui/src/main/java/com/riders/thelab/core/ui/compose/base/BaseViewModel.kt
@@ -1,12 +1,21 @@
 package com.riders.thelab.core.ui.compose.base
 
+import android.app.Activity
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import timber.log.Timber
 
 abstract class BaseViewModel : ViewModel() {
 
+    //////////////////////////////////////////
+    // Compose states
+    //////////////////////////////////////////
+    var version:String by mutableStateOf("")
+        private set
     var isDarkMode: Boolean by mutableStateOf(true)
         private set
     var isVibration: Boolean by mutableStateOf(true)
@@ -21,7 +30,28 @@ abstract class BaseViewModel : ViewModel() {
     }
 
     fun toggleVibration() {
-        isVibration = !isVibration
+        this.isVibration = !this.isVibration
     }
 
+     fun updateVersion(appVersion: String) {
+        this.version = appVersion
+    }
+
+    //////////////////////////////////////////
+    // Class Methods
+    //////////////////////////////////////////
+    fun retrieveAppVersion(activity: Activity) {
+        try {
+            val pInfo: PackageInfo =
+                activity
+                    .packageManager
+                    .getPackageInfo(activity.packageName, 0)
+            val version = pInfo.versionName
+
+            updateVersion(version)
+
+        } catch (error: PackageManager.NameNotFoundException) {
+            Timber.e(error)
+        }
+    }
 }
