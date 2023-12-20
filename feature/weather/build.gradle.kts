8d02e339887afe4c245ebcf3151ada52275321d5
MSHonore <mickael.saint-honore@chronopost.fr>
2023-12-20 23:00:14 +0100

    [develop]: quick fix WeatherActivity

--- feature/weather/build.gradle.kts
+++ feature/weather/build.gradle.kts
@@ -23,35 +23,14 @@ dependencies {
     implementation(project(":core:data"))
     implementation(project(":core:common"))
     implementation(project(":core:permissions"))
+    implementation(project(":core:ui"))
 
 
     ///////////////////////////////////
     // General Dependencies
     ///////////////////////////////////
-    // Kotlin
-    // implementation(libs.kotli)
-
-    // Compose
-    api(libs.androidx.compose.foundation)
-    api(libs.androidx.compose.foundation.layout)
-    api(libs.androidx.compose.material.iconsExtended)
-    api(libs.androidx.compose.material3)
-    api(libs.androidx.compose.runtime)
-    api(libs.androidx.compose.runtime.livedata)
-    api(libs.androidx.compose.ui.tooling.preview)
-    api(libs.androidx.compose.ui.util)
-    api(libs.androidx.metrics)
-    api(libs.androidx.tracing.ktx)
-
-    debugApi(libs.androidx.compose.ui.tooling)
-
     // AndroidX
-    implementation(libs.androidx.browser)
     implementation(libs.androidx.core.ktx)
-
-    // Coil
-    implementation(libs.coil.kt)
-    implementation(libs.coil.kt.compose)
 
     // Worker & concurrent
     api(libs.androidx.work.ktx)
