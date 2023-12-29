dab31fa9844a09ad2531d1fba81e570a0547f89d
MichaelStH <michael.sainthonore@gmail.com>
2023-12-29 16:23:58 +0100

    [feat/110-download-to-compose]: created download item

---
+++ feature/download/src/main/java/com/riders/thelab/feature/download/PreviewProvider.kt
@@ -0,0 +1,21 @@
+package com.riders.thelab.feature.download
+
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import com.riders.thelab.core.data.local.model.compose.Download
+
+class PreviewProvider : PreviewParameterProvider<Download> {
+    override val values: Sequence<Download>
+        get() = sequenceOf(
+            Download(1, "BASE_ADR_NEY_20231106_121036058.zip", 45, false, null),
+            Download(4, "test_4.zip", 100, true, null),
+            Download(
+                5,
+                "BASE_ADR_NEY_20231106_121036058.zip",
+                3,
+                true,
+                true to "Failed to download item"
+            ),
+            Download(7, "test_98.zip", 75, false, null),
+            Download(5, "BASE_ADR_NEY_20231106_121036058.zip", 100, true, null)
+        )
+}
