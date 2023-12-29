b5d4605a3b290ac9413421d82f5bade3b6a0c1e4
MichaelStH <michael.sainthonore@gmail.com>
2023-12-29 23:08:34 +0100

    [feat/110-download-to-compose]: modify permissions for download (android 13+)

--- core/data/src/main/java/com/riders/thelab/core/data/local/model/Permission.kt
+++ core/data/src/main/java/com/riders/thelab/core/data/local/model/Permission.kt
@@ -37,11 +37,21 @@ sealed class Permission(vararg val permissions: String) {
         Manifest.permission.READ_EXTERNAL_STORAGE
     )
 
+    @SuppressLint("InlinedApi")
     @RequiresApi(Build.VERSION_CODES.Q)
     data object MediaLocation :
         Permission(
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.ACCESS_MEDIA_LOCATION
+        )
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    data object MediaLocationAndroid13 :
+        Permission(
+            Manifest.permission.ACCESS_MEDIA_LOCATION,
+            Manifest.permission.READ_MEDIA_AUDIO,
+            Manifest.permission.READ_MEDIA_IMAGES,
+            Manifest.permission.READ_MEDIA_VIDEO
         )
 
     // Bundled permissions
@@ -59,12 +69,16 @@ sealed class Permission(vararg val permissions: String) {
             Manifest.permission.CAMERA -> Camera
             Manifest.permission.READ_PHONE_STATE -> PhoneState
             Manifest.permission.GET_ACCOUNTS -> UserAccounts
-            Manifest.permission.ACCESS_MEDIA_LOCATION -> MediaLocation
+            Manifest.permission.ACCESS_MEDIA_LOCATION,
+            Manifest.permission.READ_MEDIA_AUDIO,
+            Manifest.permission.READ_MEDIA_IMAGES,
+            Manifest.permission.READ_MEDIA_VIDEO -> MediaLocationAndroid13
             // Grouped permissions
             Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN -> Bluetooth
             Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN, Manifest.permission.BLUETOOTH_SCAN, Manifest.permission.BLUETOOTH_CONNECT -> BluetoothAndroid12
             Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION -> Location
             Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE -> Storage
+            Manifest.permission.ACCESS_MEDIA_LOCATION, Manifest.permission.READ_EXTERNAL_STORAGE -> MediaLocation
             else -> throw IllegalArgumentException("Unknown permission: $permission")
         }
     }
