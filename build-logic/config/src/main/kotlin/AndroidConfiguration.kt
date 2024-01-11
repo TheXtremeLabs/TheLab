2348c82a2d38977ba6789cbce10ef16a57386468
MichaelStH <michael.sainthonore@gmail.com>
2024-01-12 00:56:40 +0100

    [feat/114-weather-widget]: update to version 12.10.8

--- build-logic/config/src/main/kotlin/AndroidConfiguration.kt
+++ build-logic/config/src/main/kotlin/AndroidConfiguration.kt
@@ -10,7 +10,7 @@ object AndroidConfiguration {
 
     object Application {
         const val code: Int = 1
-        val version: Version = Version(major = 12, minor = 10, patch = 7)
+        val version: Version = Version(major = 12, minor = 10, patch = 8)
     }
 }
 
