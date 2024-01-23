259435d04eb66e72a7994f66329adf543a81c54b
Mickael Saint-Honoré <mickael.saint-honore@chronopost.fr>
2024-01-23 21:16:33 +0100

    [feat/116-theaters]: update to version 12.11.9

--- build-logic/config/src/main/kotlin/AndroidConfiguration.kt
+++ build-logic/config/src/main/kotlin/AndroidConfiguration.kt
@@ -8,7 +8,7 @@ object AndroidConfiguration {
 
     object Application {
         const val code: Int = 1
-        val version: Version = Version(major = 12, minor = 11, patch = 8)
+        val version: Version = Version(major = 12, minor = 11, patch = 9)
     }
 }
 
