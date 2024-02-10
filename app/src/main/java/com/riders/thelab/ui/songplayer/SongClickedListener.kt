ef46d9f4b163fd6581bc9ef601b109641bf5fa93
MichaelStH <michael.sainthonore@gmail.com>
2024-02-11 00:34:47 +0100

    [feat/119-song-player]: remove song player old package

--- app/src/main/java/com/riders/thelab/ui/songplayer/SongClickedListener.kt
+++
@@ -1,8 +0,0 @@
-package com.riders.thelab.ui.songplayer
-
-import android.view.View
-import com.riders.thelab.core.data.local.model.music.SongModel
-
-interface SongClickedListener {
-    fun onSongClick(view: View, item: SongModel)
-}
