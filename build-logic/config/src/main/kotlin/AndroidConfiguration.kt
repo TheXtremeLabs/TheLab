159d4ccaa45135529bb137e5088170f5c54442cc
MichaelStH <michael.sainthonore@gmail.com>
2024-02-12 18:40:10 +0100

    [develop]: update to version 12.13.8

--- build-logic/config/src/main/kotlin/AndroidConfiguration.kt
+++ build-logic/config/src/main/kotlin/AndroidConfiguration.kt
@@ -8,7 +8,7 @@ object AndroidConfiguration {
 
     object Application {
         const val code: Int = 1
-        val version: Version = Version(major = 12, minor = 13, patch = 7)
+        val version: Version = Version(major = 12, minor = 13, patch = 8)
     }
 }
 
