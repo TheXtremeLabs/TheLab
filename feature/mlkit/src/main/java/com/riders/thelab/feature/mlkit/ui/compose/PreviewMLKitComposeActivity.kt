75c2a9d5988f84e58988b74f7ac6e162dc5a741d
MichaelStH <michael.sainthonore@gmail.com>
2024-02-18 22:54:56 +0100

    [feat/122-ml-kit]: add ripple animation effects

--- feature/mlkit/src/main/java/com/riders/thelab/feature/mlkit/ui/compose/PreviewMLKitComposeActivity.kt
+++ feature/mlkit/src/main/java/com/riders/thelab/feature/mlkit/ui/compose/PreviewMLKitComposeActivity.kt
@@ -1,23 +1,41 @@
 package com.riders.thelab.feature.mlkit.ui.compose
 
 import android.annotation.SuppressLint
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.StartOffset
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import com.riders.thelab.core.ui.compose.annotation.DevicePreviews
 import com.riders.thelab.core.ui.compose.component.TheLabTopAppBar
+import com.riders.thelab.core.ui.compose.component.fab.pulsarBuilder
 import com.riders.thelab.core.ui.compose.theme.TheLabTheme
+import kotlinx.coroutines.delay
 
 
 ///////////////////////////////////////
@@ -28,6 +46,34 @@ import com.riders.thelab.core.ui.compose.theme.TheLabTheme
 @SuppressLint("UnusedBoxWithConstraintsScope")
 @Composable
 fun MLKitComposeContent() {
+    val animationDelay = 1500
+
+    val ripples = listOf(
+        remember { Animatable(initialValue = 0f) },
+        remember { Animatable(initialValue = 0f) },
+        remember { Animatable(initialValue = 0f) }
+    )
+
+    ripples.forEachIndexed { index, animatable ->
+        LaunchedEffect(Unit) {
+            // Use coroutine delay to sync animations
+            // divide the animation delay by number of circles
+            delay(timeMillis = (animationDelay / 3L) * (index + 1))
+
+            animatable.animateTo(
+                targetValue = 1f,
+                animationSpec = infiniteRepeatable(
+                    animation = tween(
+                        durationMillis = 3000,
+                        easing = LinearEasing
+                    ),
+                    initialStartOffset = StartOffset(animationDelay + 100),
+                    repeatMode = RepeatMode.Restart
+                )
+            )
+        }
+    }
+
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = { TheLabTopAppBar(withGradientBackground = true) }
@@ -40,12 +86,35 @@ fun MLKitComposeContent() {
         ) {
             Box(
                 modifier = Modifier
+                    //.size(width, height)
                     .align(Alignment.Center)
-                    .wrapContentSize()
+                    //.wrapContentSize()
                     .clip(RoundedCornerShape(16.dp)),
                 contentAlignment = Alignment.Center
             ) {
-                CameraView(modifier = Modifier.clip(RoundedCornerShape(16.dp)))
+                // animating circles
+                ripples.forEachIndexed { index, animatable ->
+                    Box(
+                        modifier = Modifier
+                            .scale(scale = animatable.value)
+                            .size(size = 500.dp)
+                            .clip(shape = RoundedCornerShape(16.dp))
+                            .background(
+                                color = Color.White.copy(alpha = (1 - animatable.value))
+                            )
+                    ) {
+                    }
+                }
+
+                CameraView(
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .clip(RoundedCornerShape(16.dp))
+                        .onGloballyPositioned {
+                            /*if (it.isAttached) {
+                                boxSize = it.size
+                            }*/
+                        })
             }
 
             Text(
