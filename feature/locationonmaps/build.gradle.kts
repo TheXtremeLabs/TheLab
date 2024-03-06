6a24a6bc9f7b875befd7775f9d9af5ea4d09ea12
Mickael Saint-Honoré <mickael.saint-honore@chronopost.fr>
2024-03-06 07:48:00 +0100

    [feat/122-location-on-maps]: create location on maps module

---
+++ feature/locationonmaps/build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    id("thelab.android.feature")
+    id("thelab.android.library.compose")
+    id("thelab.android.hilt")
+    id("thelab.android.library.jacoco")
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ksp)
+}
+
+android {
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    namespace = "com.riders.thelab.feature.weather"
+}
+
+dependencies {
+
+    ///////////////////////////////////
+    // Project
+    ///////////////////////////////////
+    implementation(project(":core:analytics"))
+    implementation(project(":core:data"))
+    implementation(project(":core:common"))
+    implementation(project(":core:permissions"))
+    implementation(project(":core:ui"))
+
+
+    ///////////////////////////////////
+    // General Dependencies
+    ///////////////////////////////////
+    // AndroidX
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.glance.appwidget)
+    implementation(libs.androidx.glance.material3)
+
+    // Worker & concurrent
+    api(libs.androidx.work.ktx)
+    api(libs.androidx.work.multiprocess)
+    api(libs.androidx.concurrent)
+    androidTestApi(libs.androidx.work.testing)
+
+    /* Hilt - We are going to use hilt.android which includes
+     * support for Activity and fragment injection so we need to include
+     * the following dependencies */
+    // Hilt
+    // The others dependencies has been added into the Hilt plugin convention class
+    implementation(libs.hilt.ext.work)
+    ksp(libs.hilt.ext.compiler)
+
+    // OkHttp: provided by data module
+
+    // Charts
+    api(libs.mpandroidchart)
+
+    androidTestImplementation(project(":core:testing"))
+}
