7b3d273119eeb88793142cdb14dcde162f77244b
Mickael Saint-Honoré <mickael.saint-honore@chronopost.fr>
2024-03-08 17:51:02 +0100

    [develop]: quick refactor

--- feature/flightaware/src/main/java/com/riders/thelab/feature/flightaware/ui/main/FlightMainActivity.kt
+++ feature/flightaware/src/main/java/com/riders/thelab/feature/flightaware/ui/main/FlightMainActivity.kt
@@ -59,7 +59,8 @@ class FlightMainActivity : BaseComponentActivity() {
                                             mViewModel.getOperator()
                                         }
                                     }
-                                })
+                                }
+                            )
                         }
                     }
                 }
