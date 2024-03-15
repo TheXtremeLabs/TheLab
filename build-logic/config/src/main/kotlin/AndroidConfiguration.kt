deb5573770e72ea5e646547ed25fdefc98fae65b
Mickael Saint-Honoré <mickael.saint-honore@chronopost.fr>
2024-03-15 21:23:07 +0100

    [develop]: update to version 12.13.15

--- build-logic/config/src/main/kotlin/AndroidConfiguration.kt
+++ build-logic/config/src/main/kotlin/AndroidConfiguration.kt
@@ -8,7 +8,7 @@ object AndroidConfiguration {
 
     object Application {
         const val CODE: Int = 1
-        val version: Version = Version(major = 12, minor = 13, patch = 14)
+        val version: Version = Version(major = 12, minor = 13, patch = 15)
     }
 }
 
