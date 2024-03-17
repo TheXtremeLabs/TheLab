bd00137b993864553e3f459aed70bee9d442b8b7
MichaelStH <michael.sainthonore@gmail.com>
2024-03-17 14:38:01 +0100

    [develop]: refactor signup screen (for previews)

--- app/src/main/java/com/riders/thelab/ui/signup/SignUpActivity.kt
+++ app/src/main/java/com/riders/thelab/ui/signup/SignUpActivity.kt
@@ -6,8 +6,10 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.riders.thelab.core.ui.compose.base.BaseComponentActivity
@@ -15,6 +17,8 @@ import com.riders.thelab.core.ui.compose.theme.TheLabTheme
 import com.riders.thelab.navigator.Navigator
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+import kotools.types.experimental.ExperimentalKotoolsTypesApi
+import kotools.types.web.EmailAddress
 import timber.log.Timber
 
 @AndroidEntryPoint
@@ -27,6 +31,7 @@ class SignUpActivity : BaseComponentActivity() {
     // OVERRIDE
     //
     /////////////////////////////////////
+    @OptIn(ExperimentalKotoolsTypesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -34,13 +39,43 @@ class SignUpActivity : BaseComponentActivity() {
             Timber.d("coroutine launch with name ${this.coroutineContext}")
             repeatOnLifecycle(Lifecycle.State.CREATED) {
                 setContent {
+                    val userState by mViewModel.userState.collectAsStateWithLifecycle()
+                    val emailHasError by mViewModel.emailHasError.collectAsStateWithLifecycle()
+                    val passwordsHasError by mViewModel.passwordsHasError.collectAsStateWithLifecycle()
+
                     TheLabTheme(mViewModel.isDarkMode) {
                         // A surface container using the 'background' color from the theme
                         Surface(
                             modifier = Modifier.fillMaxSize(),
                             color = MaterialTheme.colorScheme.background
                         ) {
-                            SignUpContent(viewModel = mViewModel)
+                            SignUpContent(
+                                isDarkMode = mViewModel.isDarkMode,
+                                currentDestination = mViewModel.currentDestination,
+                                onUpdateCurrentNavDestination = mViewModel::updateCurrentNavDestination,
+                                userUiState = userState,
+                                emailHasError = emailHasError,
+                                passwordsHasError = passwordsHasError,
+                                firstname = mViewModel.firstname,
+                                onUpdateFirstname = mViewModel::updateFirstname,
+                                lastname = mViewModel.lastname,
+                                onUpdateLastname = mViewModel::updateLastname,
+                                username = mViewModel.username,
+                                onUpdateUsername = mViewModel::updateUsername,
+                                email = EmailAddress.create(mViewModel.email),
+                                onUpdateEmail = { mViewModel::updateEmail },
+                                password = mViewModel.password,
+                                onUpdatePassword = mViewModel::updatePassword,
+                                passwordConfirmation = mViewModel.passwordConfirmation,
+                                onUpdatePasswordConfirmation = mViewModel::updatePasswordConfirmation,
+                                shouldShowExitDialogConfirmation = mViewModel.shouldShowExitDialogConfirmation,
+                                onUpdateShouldShowExitDialogConfirmation = mViewModel::updateShouldShowExitDialogConfirmation,
+                                userFormButtonEnabled = mViewModel.userFormButtonEnabled,
+                                onSubmitForm = mViewModel::submitForm,
+                                isSubmitSuccess = mViewModel.isSubmitSuccess,
+                                message = mViewModel.message,
+                                shouldShowSaveOrErrorView = mViewModel.shouldShowSaveOrErrorView
+                            )
                         }
                     }
                 }
@@ -59,70 +94,6 @@ class SignUpActivity : BaseComponentActivity() {
     // CLASSES METHODS
     //
     /////////////////////////////////////
-
-    /*@SuppressLint("NewApi")
-    private fun updateToolbarUserForm() {
-        Timber.d("updateToolbarUserForm()")
-        if (binding.includeToolbarSignUpLayout.progressBarUserForm.progress == 0) {
-            binding.includeToolbarSignUpLayout.progressBarUserForm.setProgress(100, true)
-        }
-
-        if (ContextCompat.getColor(
-                this@SignUpActivity,
-                R.color.white
-            ) != binding.includeToolbarSignUpLayout.tvUserForm.textColors.defaultColor
-        ) {
-            binding.includeToolbarSignUpLayout.tvUserForm.setTextColor(
-                ContextCompat.getColor(
-                    this@SignUpActivity,
-                    R.color.white
-                )
-            )
-        }
-    }
-
-    @SuppressLint("NewApi")
-    private fun removeToolbarUserForm() {
-        Timber.d("removeToolbarUserForm()")
-        if (binding.includeToolbarSignUpLayout.progressBarUserForm.progress == 100) {
-            binding.includeToolbarSignUpLayout.progressBarUserForm.setProgress(0, true)
-        }
-
-        if (ContextCompat.getColor(
-                this@SignUpActivity,
-                R.color.white
-            ) == binding.includeToolbarSignUpLayout.tvUserForm.textColors.defaultColor
-        ) {
-            binding.includeToolbarSignUpLayout.tvUserForm.setTextColor(
-                ContextCompat.getColor(
-                    this@SignUpActivity,
-                    R.color.jumbo
-                )
-            )
-        }
-    }
-
-    @SuppressLint("NewApi")
-    private fun updateToolbarSuccessful() {
-        Timber.d("updateToolbarSuccessful()")
-        if (binding.includeToolbarSignUpLayout.progressBarSuccessful.progress == 0) {
-            binding.includeToolbarSignUpLayout.progressBarSuccessful.setProgress(100, true)
-        }
-
-        if (ContextCompat.getColor(
-                this@SignUpActivity,
-                R.color.white
-            ) != binding.includeToolbarSignUpLayout.tvSuccessful.textColors.defaultColor
-        ) {
-            binding.includeToolbarSignUpLayout.tvSuccessful.setTextColor(
-                ContextCompat.getColor(
-                    this@SignUpActivity,
-                    R.color.white
-                )
-            )
-        }
-    }*/
-
     fun launchMainActivity() {
         Timber.d("launchMainActivity()")
         Navigator(this).callMainActivity()
