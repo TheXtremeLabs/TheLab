b94a6ed7c159be242e331de388094674fd4d9eee
MichaelStH <michael.sainthonore@gmail.com>
2025-06-12 17:14:01 +0200

    [develop]: upgrade to version 13.1.0

--- build-logic/config/src/main/kotlin/AndroidConfiguration.kt
+++ build-logic/config/src/main/kotlin/AndroidConfiguration.kt
@@ -8,7 +8,7 @@ object AndroidConfiguration {
 
     object Application {
         const val CODE: Int = 1
-        val version: Version = Version(major = 13, minor = 0, patch = 3)
+        val version: Version = Version(major = 13, minor = 1, patch = 0)
     }
 }
 
