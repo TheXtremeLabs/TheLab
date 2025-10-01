1bc01e8212e622760d0876fbf7da194e438ba859
Mickael Saint-Honoré <mickael.saint-honore@chronopost.fr>
2025-10-01 17:57:28 +0200

    [develop]: quick changes

--- core/common/src/main/java/com/riders/thelab/core/common/storage/LabStorageManager.kt
+++ core/common/src/main/java/com/riders/thelab/core/common/storage/LabStorageManager.kt
@@ -1,5 +1,34 @@
 package com.riders.thelab.core.common.storage
 
+import android.content.Context
+import android.os.Environment
+import timber.log.Timber
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileOutputStream
+import java.io.FileReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+
 class LabStorageManager() {
+
+    @JvmStatic
+    fun isExternalStorageReadOnly(): Boolean {
+        val extStorageState = Environment.getExternalStorageState()
+        if (Environment.MEDIA_MOUNTED_READ_ONLY == extStorageState) {
+            return true
+        }
+        return false
+    }
+
+    @JvmStatic
+    fun isExternalStorageAvailable(): Boolean {
+        val extStorageState = Environment.getExternalStorageState()
+        if (Environment.MEDIA_MOUNTED == extStorageState) {
+            return true
+        }
+        return false
+    }
 
 }
