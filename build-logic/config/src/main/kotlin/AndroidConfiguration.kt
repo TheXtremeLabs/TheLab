a9623feed67b15e00af9632e1385c415319eb414
MichaelStH <michael.sainthonore@gmail.com>
2025-10-06 13:44:00 +0200

    [develop]: update to version 14.0.2

--- build-logic/config/src/main/kotlin/AndroidConfiguration.kt
+++ build-logic/config/src/main/kotlin/AndroidConfiguration.kt
@@ -8,7 +8,7 @@ object AndroidConfiguration {
 
     object Application {
         const val CODE: Int = 1
-        val version: Version = Version(major = 14, minor = 0, patch = 1)
+        val version: Version = Version(major = 14, minor = 0, patch = 2)
     }
 }
 
