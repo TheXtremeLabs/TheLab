200e73993adc579f7595c3a645fc32afa4dee18b
Mickael Saint-Honoré <mickael.saint-honore@chronopost.fr>
2025-11-04 17:16:23 +0100

    [develop] : add central module

---
+++ central/build.gradle.kts
@@ -0,0 +1,93 @@
+plugins {
+    alias(libs.plugins.thelab.application)
+    alias(libs.plugins.thelab.application.compose)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+}
+
+val versionMajor: Int = 0
+val versionMinor: Int = 0
+val versionPatch: Int = 1
+
+/**
+ * Log events in console
+ *
+ * @param tag
+ * @param message
+ * @return
+ */
+fun log(tag: String, message: String) {
+    println("---> KotlinDSL script logs | $tag | $message")
+}
+
+android {
+    namespace = "com.riders.thelab.central"
+    compileSdk {
+        version = release(36)
+    }
+
+    defaultConfig {
+        applicationId = "com.riders.thelab.central"
+
+        versionCode = 2000 * versionMajor + 100 * versionMinor + versionPatch
+        versionName = "$versionMajor.$versionMinor.$versionPatch"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    packaging {
+        resources {
+            excludes += "META-INF/DEPENDENCIES"
+            excludes += "META-INF/INDEX.LIST"
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+
+
+    ///////////////////////////////////
+    // Project
+    ///////////////////////////////////
+    implementation(project(":core:common"))
+    implementation(project(":core:permissions"))
+    implementation(project(":core:ui"))
+
+
+    /////////////////////////////
+    // General Dependencies
+    /////////////////////////////
+    // AndroidX
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+
+
+    /////////////////////////////
+    // Tests Dependencies
+    /////////////////////////////
+    testImplementation(libs.junit4)
+    androidTestImplementation(libs.androidx.test.junit)
+    androidTestImplementation(libs.androidx.test.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.testManifest)
+}
