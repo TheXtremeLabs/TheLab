01bc03a37d644420da5b3880cc9b2ffe750aa991
MichaelStH <michael.sainthonore@gmail.com>
2025-11-07 17:04:23 +0100

    [develop] : refactor UiEvent class

--- feature/songplayer/src/main/java/com/riders/thelab/feature/songplayer/ui/UiEvent.kt
+++ feature/songplayer/src/main/java/com/riders/thelab/feature/songplayer/ui/UiEvent.kt
@@ -1,8 +1,8 @@
 package com.riders.thelab.feature.songplayer.ui
 
 sealed interface UiEvent {
-    data class OnSongItemClicked(val songId : Int ): UiEvent
-    data class OnPlayerCardClicked(val expanded : Boolean ): UiEvent
+    data class OnSongItemClicked(val songId: Int) : UiEvent
+    data class OnPlayerCardClicked(val expanded: Boolean) : UiEvent
 
     data object OnNextClicked : UiEvent
     data object OnPlayPauseClicked : UiEvent
