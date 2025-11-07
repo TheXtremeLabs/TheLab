1282110029f2a2286339566eff75e229e145de98
MichaelStH <michael.sainthonore@gmail.com>
2025-11-07 14:40:59 +0100

    [develop] : add UiEvent in SongPlayer module

---
+++ feature/songplayer/src/main/java/com/riders/thelab/feature/songplayer/ui/UiEvent.kt
@@ -0,0 +1,10 @@
+package com.riders.thelab.feature.songplayer.ui
+
+sealed interface UiEvent {
+    data class OnSongItemClicked(val songId : Int ): UiEvent
+    data class OnPlayerCardClicked(val expanded : Boolean ): UiEvent
+
+    data object OnNextClicked : UiEvent
+    data object OnPlayPauseClicked : UiEvent
+    data object OnPreviousClicked : UiEvent
+}
