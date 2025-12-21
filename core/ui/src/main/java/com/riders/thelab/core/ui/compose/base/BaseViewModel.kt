2a8daa60099a0b9bbdaa62c0b7f14c248fb6ae45
MichaelStH <michael.sainthonore@gmail.com>
2025-12-21 16:04:52 +0100

    [develop] : get ui repository values in baseviewmodel

--- core/ui/src/main/java/com/riders/thelab/core/ui/compose/base/BaseViewModel.kt
+++ core/ui/src/main/java/com/riders/thelab/core/ui/compose/base/BaseViewModel.kt
@@ -1,9 +1,7 @@
 package com.riders.thelab.core.ui.compose.base
 
 import android.annotation.SuppressLint
-import android.app.Activity
 import android.content.pm.PackageInfo
-import android.content.pm.PackageManager
 import androidx.activity.ComponentActivity
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -14,6 +12,16 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.riders.thelab.core.ui.compose.data.AppTheme
+import com.riders.thelab.core.ui.data.local.IUiRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotools.types.text.NotBlankString
+import kotools.types.text.toNotBlankString
 import timber.log.Timber
 import java.lang.ref.WeakReference
 
@@ -41,7 +49,7 @@ fun <viewModel : LifecycleObserver> viewModel.observeLifecycleEvents(lifecycle: 
 }
 
 @Suppress("EmptyMethod")
-abstract class BaseViewModel : ViewModel() {
+open class BaseViewModel(val uiRepository: IUiRepository) : ViewModel() {
 
     var mWeakReference: WeakReference<ComponentActivity>? = null
         private set
@@ -49,16 +57,53 @@ abstract class BaseViewModel : ViewModel() {
     //////////////////////////////////////////
     // Compose states
     //////////////////////////////////////////
-    var version: String by mutableStateOf("")
-        private set
-    var isDarkMode: Boolean by mutableStateOf(true)
-        private set
-    var isVibration: Boolean by mutableStateOf(true)
-        private set
-    private var isFirstTimeLaunched: Boolean by mutableStateOf(true)
-        private set
-    var isActivitiesSplashEnabled: Boolean by mutableStateOf(true)
-        private set
+    // --- Version
+    private var _version: MutableStateFlow<String> = MutableStateFlow("")
+    open var version: StateFlow<String> = _version
+
+    // --- Is First Time Launched
+    private var _isFirstTimeLaunched: MutableStateFlow<Boolean> = MutableStateFlow(true)
+    val isFirstTimeLaunched: StateFlow<Boolean> by lazy {
+        uiRepository
+            .getIsFirstTimeLaunched()
+            .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5_000L), true)
+    }
+
+    // --- Theme
+    private var _theme: MutableStateFlow<AppTheme> = MutableStateFlow(AppTheme.Default)
+    val theme: StateFlow<AppTheme> by lazy {
+        uiRepository
+            .getThemeColorAsAppTheme()
+            .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5_000L), AppTheme.Default)
+    }
+
+    // --- Dark Mode
+    private var _isDarkMode: MutableStateFlow<Boolean?> = MutableStateFlow(null)
+    val isDarkMode: StateFlow<Boolean?> by lazy {
+        uiRepository
+            .isDarkTheme()
+            .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5_000L), null)
+    }
+
+    // --- Vibration
+    private var _isVibration: MutableStateFlow<Boolean> = MutableStateFlow(true)
+    val isVibration: StateFlow<Boolean> by lazy {
+        uiRepository
+            .getIsVibrationEnabled()
+            .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5_000L), true)
+    }
+
+    // --- Activities Splash
+    private var _isActivitiesSplashEnabled: MutableStateFlow<Boolean> = MutableStateFlow(true)
+    val isActivitiesSplashEnabled: StateFlow<Boolean> by lazy {
+        uiRepository
+            .getIsActivitiesSplashEnabled()
+            .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5_000L), true)
+    }
+
+    //////////////////////////////////////////
+    // Properties
+
     private var isUserLoggedIn: Boolean by mutableStateOf(false)
         private set
     var viewPagerDotExpanded: Boolean by mutableStateOf(true)
@@ -68,25 +113,6 @@ abstract class BaseViewModel : ViewModel() {
     private var viewPagerCurrentIndex: Int by mutableIntStateOf(0)
         private set
 
-    fun updateDarkMode(darkMode: Boolean) {
-        Timber.d("updateDarkMode() | darkMode: $darkMode")
-        this.isDarkMode = darkMode
-    }
-
-    fun updateVibration(isVibration: Boolean) {
-        Timber.d("updateVibration() | isVibration: $isVibration")
-        this.isVibration = isVibration
-    }
-
-    fun updateFirstTimeLaunched(firstTimeLaunched: Boolean) {
-        Timber.d("updateFirstTimeLaunched() | is first Time Launched: $firstTimeLaunched")
-        this.isFirstTimeLaunched = firstTimeLaunched
-    }
-
-    fun updateActivitiesSplashEnabled(isSplashEnabled: Boolean) {
-        Timber.d("updateActivitiesSplashEnabled() | isSplashEnabled: $isSplashEnabled")
-        this.isActivitiesSplashEnabled = isSplashEnabled
-    }
 
     fun updateIsUserLoggedIn(isUserLoggedIn: Boolean) {
         Timber.d("updateIsUserLoggedIn() | isUserLoggedIn: $isUserLoggedIn")
@@ -94,7 +120,33 @@ abstract class BaseViewModel : ViewModel() {
     }
 
     private fun updateVersion(appVersion: String) {
-        this.version = appVersion
+        Timber.d("updateVersion() | appVersion: $appVersion")
+        this._version.update { appVersion }
+    }
+
+    fun updateIsFirstTimeLaunched(isFirstTimeLaunched: Boolean) {
+        Timber.d("updateIsFirstTimeLaunched() | isFirstTimeLaunched: $isFirstTimeLaunched")
+        this._isFirstTimeLaunched.update { isFirstTimeLaunched }
+    }
+
+    fun updateTheme(theme: AppTheme) {
+        Timber.d("updateTheme() | theme: $theme")
+        this._theme.update { theme }
+    }
+
+    fun updateIsDarkMode(isDarkMode: Boolean) {
+        Timber.d("updateIsDarkMode() | isDarkMode: $isDarkMode")
+        this._isDarkMode.update { isDarkMode }
+    }
+
+    fun updateIsVibration(isVibration: Boolean) {
+        Timber.d("updateIsVibration() | isVibration: $isVibration")
+        this._isVibration.update { isVibration }
+    }
+
+    fun updateIsActivitiesSplashEnabled(isActivitiesSplashEnabled: Boolean) {
+        Timber.d("updateIsActivitiesSplashEnabled() | isActivitiesSplashEnabled: $isActivitiesSplashEnabled")
+        this._isActivitiesSplashEnabled.update { isActivitiesSplashEnabled }
     }
 
     fun updateViewPagerExpanded(expanded: Boolean) {
@@ -118,18 +170,39 @@ abstract class BaseViewModel : ViewModel() {
         }
     }
 
-    fun retrieveAppVersion(activity: Activity) {
-        try {
-            val pInfo: PackageInfo =
-                activity
+    @Throws(Exception::class)
+    fun getAppVersion(): NotBlankString? {
+        if (null == mWeakReference) {
+            Timber.e("getAppVersion() | WeakReference is null")
+            return null
+        }
+
+        if (version.value.trim().isNotBlank()) {
+            return version.value.toNotBlankString().getOrThrow()
+        }
+
+        return mWeakReference?.get()?.let { activity ->
+            runCatching {
+                Timber.d("getAppVersion()")
+                val pInfo: PackageInfo = activity
                     .packageManager
                     .getPackageInfo(activity.packageName, 0)
-            val version = pInfo.versionName
-
-            version?.let { updateVersion(it) }
-
-        } catch (error: PackageManager.NameNotFoundException) {
-            Timber.e("retrieveAppVersion() | Error caught: $error")
+                return@runCatching pInfo.versionName?.toNotBlankString()?.getOrThrow()
+                    ?.also { version ->
+                        updateVersion(version.toString())
+                    }
+                    ?: throw Exception("Version is null")
+            }
+                .onFailure { exception ->
+                    Timber.e("getAppVersion() | Error caught with message: ${exception.message} (${exception.javaClass.canonicalName})")
+                }
+                .onSuccess { version ->
+                    Timber.d("getAppVersion() | version: $version")
+                }
+                .getOrThrow()
+        } ?: run {
+            Timber.e("getAppVersion() | WeakReference is null")
+            return null
         }
     }
 }
