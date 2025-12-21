12e0f37a76cbb05c068c2ff7d133014a89ee819f
MichaelStH <michael.sainthonore@gmail.com>
2025-12-21 15:58:23 +0100

    [develop] : refactor preferences for ui using protobuf datastore

---
+++ core/ui/src/main/java/com/riders/thelab/core/ui/data/local/preferences/serializer/UserPreferencesSerializer.kt
@@ -0,0 +1,24 @@
+package com.riders.thelab.core.ui.data.local.preferences.serializer
+
+import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.Serializer
+import com.google.protobuf.InvalidProtocolBufferException
+import com.riders.thelab.core.ui.data.local.preferences.proto.UserPreferences
+import java.io.InputStream
+import java.io.OutputStream
+
+object UserPreferencesSerializer : Serializer<UserPreferences> {
+    override val defaultValue: UserPreferences
+        get() = UserPreferences.getDefaultInstance()
+
+    override suspend fun readFrom(input: InputStream): UserPreferences = try {
+        UserPreferences.parseFrom(input)
+    } catch (exception: InvalidProtocolBufferException) {
+        throw CorruptionException("Cannot read proto.", exception)
+    }
+
+    override suspend fun writeTo(
+        t: UserPreferences,
+        output: OutputStream
+    ) = t.writeTo(output)
+}
