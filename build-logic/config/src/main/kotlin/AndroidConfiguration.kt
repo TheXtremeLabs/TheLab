cc548c2caed892cdf8a87ca26f57451c4426cbf8
MichaelStH <michael.sainthonore@gmail.com>
2026-05-04 19:12:02 +0200

    [develop] : chore upgrade to version 14.1.1

--- build-logic/config/src/main/kotlin/AndroidConfiguration.kt
+++ build-logic/config/src/main/kotlin/AndroidConfiguration.kt
@@ -10,7 +10,7 @@ object AndroidConfiguration {
 
     object Application {
         const val CODE: Int = 1
-        val version: Version = Version(major = 14, minor = 1, patch = 0)
+        val version: Version = Version(major = 14, minor = 1, patch = 1)
     }
 }
 
